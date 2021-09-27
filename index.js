406de4dbc6635c840d79578795c2c5dd3869c20e
adrianvh33 <adrian.valencia02@usc.edu.co>
2021-09-27 09:30:37 -0500

    prueba de jenkins 2

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const userRoutes = require('./src/controllers/user.controller')
 
 // Set route path on project
 app.get('/', async (req, res) => {
-  res.status(200).json("App is running! OK")
+  res.status(200).json("App is running! OKay")
 })
 
 app.use('/users', userRoutes)
