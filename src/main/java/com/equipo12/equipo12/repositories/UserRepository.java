79d124c584d59f2c1e29527020520ba184156b25
adrianvh33 <adrian.valencia02@usc.edu.co>
2021-10-09 10:23:35 -0500

    pruebas

--- src/main/java/com/equipo12/equipo12/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.equipo12.equipo12.repositories;
-
-import com.equipo12.equipo12.models.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository <User, String>{
-
-}
