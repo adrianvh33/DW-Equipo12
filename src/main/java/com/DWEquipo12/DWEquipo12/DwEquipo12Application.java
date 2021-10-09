25c2974d18a5b03a01a871b08b787815f45de70e
adrianvh33 <adrian.valencia02@usc.edu.co>
2021-10-09 10:29:06 -0500

    paquete  inicial

---
+++ src/main/java/com/DWEquipo12/DWEquipo12/DwEquipo12Application.java
@@ -0,0 +1,13 @@
+package com.DWEquipo12.DWEquipo12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwEquipo12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwEquipo12Application.class, args);
+	}
+
+}
