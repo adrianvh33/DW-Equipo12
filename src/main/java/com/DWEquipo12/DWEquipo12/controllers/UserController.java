7b2309a75189c489b46127c4209c0e3eee3e343a
adrianvh33 <adrian.valencia02@usc.edu.co>
2021-10-12 11:51:13 -0500

    se agrega eliminar usuario de projecto

--- src/main/java/com/DWEquipo12/DWEquipo12/controllers/UserController.java
+++ src/main/java/com/DWEquipo12/DWEquipo12/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.DWEquipo12.DWEquipo12.controllers;
 
 import com.DWEquipo12.DWEquipo12.models.Project;
 import com.DWEquipo12.DWEquipo12.models.User;
+import com.DWEquipo12.DWEquipo12.repositories.ProjectRepository;
 import com.DWEquipo12.DWEquipo12.repositories.UserRepository;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,9 +15,11 @@ import java.util.Optional;
 public class UserController {
 
     private final UserRepository userRepository;
+    private final ProjectRepository projectRepository;
 
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository,ProjectRepository projectRepository) {
         this.userRepository = userRepository;
+        this.projectRepository = projectRepository;
         List<String> id_proyectos = new ArrayList<>();
         User newUser01 = new User("001",1144088597,"usuario","prueba1","p1@gmai.com"
                                  ,"123456","estudiante"
@@ -36,19 +39,39 @@ public class UserController {
     /*
     * Se agrega un id de proyecto a un usuario
      */
-    @GetMapping("/setProject/{userId}/{projecId}")
-    User setProject(@PathVariable String userId,@PathVariable String projecId){
+    @GetMapping("/projec2User/{userId}/{projectId}")
+    User projec2User(@PathVariable String userId,@PathVariable String projectId){
         User user = userRepository.findById(userId).orElse(null); // se busca el usuario por su id
-        List<String> proyecto = new ArrayList<>(user.getId_proyectos()); // se crea una lista a la que se le copia los proyectos que el usuario ya tiene
-        proyecto.add(projecId); // Se agrega el nuevo id de pryecto a la lista
-        user.setId_proyectos(proyecto); //Se agrega la lista al usuario
+        List<String> proyectoL = new ArrayList<>(user.getId_proyectos()); // se crea una lista a la que se le copia los proyectos que el usuario ya tiene
+        proyectoL.add(projectId); // Se agrega el nuevo id de pryecto a la lista
+        user.setId_proyectos(proyectoL); //Se agrega la lista al usuario
+        Project project = projectRepository.findById(projectId).orElse(null);
+        List<String> userL = new ArrayList<>(project.getIntegrantes());
+        userL.add(userId);
+        project.setIntegrantes(userL);
+        projectRepository.save(project);
+        return userRepository.save(user); // se guarda el usuario
+    }
+
+
+    @PutMapping("/reProject/{userId}/{projectId}")
+    User reProject(@PathVariable String userId,@PathVariable String projectId){
+        User user = userRepository.findById(userId).orElse(null); // se busca el usuario por su id
+        Project project = projectRepository.findById(projectId).orElse(null);
+        List<String> userL = new ArrayList<>(project.getIntegrantes());
+        List<String> proyectoL = new ArrayList<>(user.getId_proyectos()); // se crea una lista a la que se le copia los proyectos que el usuario ya tiene
+        userL.remove(userId);
+        proyectoL.remove(projectId); // Se agrega el nuevo id de pryecto a la lista
+        project.setIntegrantes(userL);
+        user.setId_proyectos(proyectoL); //Se agrega la lista al usuario
+        projectRepository.save(project);
         return userRepository.save(user); // se guarda el usuario
     }
 
     /*
     *Se crea un nuevo usuario
      */
-    @PostMapping("/newUser") // se usa una peticion post para pedir un cuerpo Json con los datos del usuario
+    @PostMapping("/user") // se usa una peticion post para pedir un cuerpo Json con los datos del usuario
     User newUser(@RequestBody User user){
         user.setFecha_ingreso(new Date()); // se agrega la fecha al usuario
         return userRepository.save(user); // seguarda el usuario
